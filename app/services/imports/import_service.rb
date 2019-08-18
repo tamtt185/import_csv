e0cef1b16e0b2416e8c053b0a1f34b874f0b7700
tamtt-1031 <tran.thanh.tam@sun-asterisk.com>
2019-08-18 17:58:43 +0700

    Convert field data

--- app/services/imports/import_service.rb
+++ app/services/imports/import_service.rb
@@ -5,6 +5,11 @@ class Imports::ImportService < BaseService
     "Gender" => "gender"
   }
 
+  GENDER_HASH = {
+    "Nam" => "male",
+    "Nu" => "female"
+  }
+
   def initialize **args
     # @log_file = args[:log_file]
     @file_name = args[:file_name]
@@ -17,6 +22,16 @@ class Imports::ImportService < BaseService
 
     @header = get_header
     return unless check_header_file?
+
+    columns = @header.map{|col| HEADER_HASH[col]}
+    CSV.foreach(file_path, headers: false).drop(1).each.with_index(1) do |row, index|
+      row = row.map{|value| value.squish}
+      binding.pry
+      row = [columns, row.map{|value| value.squish}].transpose.to_h
+      row["gender"] = convert_gender row["gender"]
+      binding.pry
+      User.new row
+    end
   end
 
   private
@@ -39,5 +54,9 @@ class Imports::ImportService < BaseService
     @errors_count += 1
     @log_file.error "Import fail, Headers of file invalid"
     false
+  end
+
+  def convert_gender gender
+     GENDER_HASH[gender]
   end
 end
